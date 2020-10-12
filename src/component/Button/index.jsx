1abfaa4680fbce488088684e7c923388616a8cf4
Wirya Ramadhan <AdhanIsCuber@gmail.com>
2020-10-13 01:14:13 +0700

    update

--- src/component/Button/index.jsx
+++ src/component/Button/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { color, layout, space } from "styled-system";
+import { color, layout, space, borderRadius } from "styled-system";
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
+  font-size: 16px;
+  background-color: red;
   ${color}
   ${layout}
   ${space}
+  ${borderRadius}
 `;
 
-const Button = ({ children, ...props }) => (
-  <ButtonStyled {...props}>{children}</ButtonStyled>
+const Button = ({ children, m = 16, p = 16, ...props }) => (
+  <ButtonStyled m={m} p={p} {...props}>
+    {children}
+  </ButtonStyled>
 );
 
 export default Button;
+
+// w={[ 1, 1 / 2, 1 / 4 ]}
+// w={[ 1, '260px', '10em' ]}
+// theme -> w={{ _: 1, sm: 1, md: 1 / 2, lg: 1 / 4 }}
